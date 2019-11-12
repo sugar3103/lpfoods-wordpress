e951a9bea8c32f933cc6848a629f7122af1090d2
sugar3103 <duong16628@gmail.com>
2019-11-12 10:40:59 +0700

    first time add project with full site

---
+++ app/uploads/cs-branding-uploads/cs-info-page/cs-info-page.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title><br />
+<b>Notice</b>:  Undefined variable: pageTitle in <b>/var/www/clients/client3/web122/web/lpfoods-com-sg/site/web/app/uploads/cs-branding-uploads/cs-info-page/cs-info-page.php</b> on line <b>13</b><br />
+</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="manifest" href="/app/uploads/cs-branding-uploads/cs-info-page/site.webmanifest">
+<link rel="apple-touch-icon" href="/app/uploads/cs-branding-uploads/cs-info-page/icon.png">
+<link rel="stylesheet" href="/app/uploads/cs-branding-uploads/cs-info-page/main.css">
+</head>
+<body>
+<main>
+<section class="secton section-maintenance" style="background-image: url(https://lpfoods.conceptual.site/app/uploads/cs-branding-uploads/cs-info-page/filter.png);">
+<div class="maintenance-background"><img src="/app/uploads/cs-branding-uploads/cs-info-page/background.png"></div>
+<div class="d-flex flex-column justify-content-center align-items-center h-100 px-5">
+<div class="row main-content-wrapper justify-content-center align-items-center">
+<a href="https://conceptual.studio/"><img class="main-logo" src="/app/uploads/cs-branding-uploads/cs-info-page/ic-logo.svg"></a>
+<div class="main-content">
+<h1 class="maintenance-title"><br />
+<b>Notice</b>: Undefined variable: pageTitle in <b>/var/www/clients/client3/web122/web/lpfoods-com-sg/site/web/app/uploads/cs-branding-uploads/cs-info-page/cs-info-page.php</b> on line <b>33</b><br />
+</h1>
+<p class="maintenance-subtitle"><br />
+<b>Notice</b>: Undefined variable: pageDescription in <b>/var/www/clients/client3/web122/web/lpfoods-com-sg/site/web/app/uploads/cs-branding-uploads/cs-info-page/cs-info-page.php</b> on line <b>34</b><br />
+</p>
+</div>
+</div>
+<div class="row maintenance-footer justify-content-center">
+<p class="text-gray small">2019 &copy; All rights reserved. Designed by Conceptual Studio.</p>
+</div>
+</div>
+</section>
+</main>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous" type="4885f9ccffff205ae673579c-text/javascript"></script>
+<script src="/app/uploads/cs-branding-uploads/cs-info-page/main.js" type="4885f9ccffff205ae673579c-text/javascript"></script>
+<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/95c75768/cloudflare-static/rocket-loader.min.js" data-cf-settings="4885f9ccffff205ae673579c-|49" defer=""></script></body>
+</html>
